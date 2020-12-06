d0cc46bca722c29d73fb63d2cebd6f7fe495ecab
Parth Saravade <73926727+PsychedelicOrange@users.noreply.github.com>
2020-12-06 15:48:48 +0530

    Uploaded Q4

--- Question_Four.cpp
+++ Question_Four.cpp
@@ -1,28 +1,48 @@
 #include <iostream>
 int main()
 {
+    int m,n;
+    std::cout << "PLEASE ENTER ROWS IN MATRIX";
+    std::cin >> m;
+    std::cout << "PLEASE ENTER COLUMNS IN MATRIX";
+    std::cin >> n;
 
-std::string x;
-std::cin>>x;
-int i{0};
-int k{0};
-bool isHill{1};
-while( i<(x.length()))
-{  if( x[i]>x[i+1] )
-    {
-        k=1;
-    }
-    else
-    {
-        if(k==1)
-        {
-            isHill=0;
-        }
-    }
-    i++;
-}
-std::cout<<isHill;
-return 0;
+     //creating pointer to array of pointer
+    int** a{ new int* [n] };
+    //assigning array to pointer
+    for (int i = 0; i < n; i++)
+        a[i] = new int[m];
+    std::cout<<"PLEASE ENTER VALUES FOR MATRIX ( "<<m<<"X"<<n<<" ) ROW WISE, FROM LEFT TO RIGHT:";
+    //asigning values to a
+    for (int j=0;j<m;j++)
+        for (int i=0;i<n;i++)
+            std::cin>>a[i][j];
+    int* icount = new int[n*m-1];
+    int* jcount = new int[n*m-1];
+    int ii{0};
+    //processing
+    for (int j=0;j<m;j++)
+        for (int i=0;i<n;i++)
+            if(a[i][j]==0)
+                {
+                    icount[ii]=i;
+                    jcount[ii]=j;
+                    ii++;
+                }
 
+    for(int i =0;i<ii;i++)
+        for(int k=0;k<m;k++)
+            a[icount[i]][k]=0;      
+    for(int i =0;i<ii;i++)
+        for(int k=0;k<m;k++)
+            a[k][jcount[i]]=0;
+    //print matrix
+    for (int j=0;j<m;j++)
+        {   for(int i=0;i<n;i++)
+                std::cout<<a[i][j]<<" "; 
+            std::cout<<"\n";
+            
+        }      
+    return 0;
 
 }
