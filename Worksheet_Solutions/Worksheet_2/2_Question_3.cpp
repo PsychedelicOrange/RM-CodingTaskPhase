f136b03676acba68e50666ceb54eecde758b1441
ParthSaravade <parthsaravade.acad@gmail.com>
2020-12-09 12:21:19 +0530

    added 2_Q_3

---
+++ Worksheet_Solutions/Worksheet_2/2_Question_3.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstring>
+
+int length(const char* x)
+{
+    int i{0};
+    while(*x!='\0')
+    {
+        x=x+1;
+        i++;
+    }
+    return i;
+}
+void copy(char* y, const char* x)
+{
+    while(*x!='\0')
+    {
+        *y=*x;
+        y++;x++;
+    }
+
+}
+int main()
+{   
+    
+    std::string a;
+    std::cout<<"Enter String:";
+    std::cin>>a;
+    const char* a_ = a.c_str();
+    char* b = new char[length(a_)];
+    copy(b,a_);
+
+    std::cout<<b;
+
+
+    delete[] b;
+    return 0;
+    
+    
+}
