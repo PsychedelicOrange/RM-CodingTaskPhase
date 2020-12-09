8763535ebde2b27d46455941b851cb23033b4511
ParthSaravade <parthsaravade.acad@gmail.com>
2020-12-09 11:57:15 +0530

    added second question

---
+++ Worksheet_Solutions/Worksheet_2/2_Question_2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+
+int length(const char* x)
+{
+    int i{0};
+    while(*x!='\0')
+    {
+        x=x+1;
+        i++;
+    }
+    return i;
+}
+int main()
+{   
+    
+    std::string a;
+    std::cout<<"Enter String:";
+    std::cin>>a;
+    const char* b = a.c_str();
+    std::cout<<"Length of string is "<<length(b)<<".";
+    return 0;
+    
+}
